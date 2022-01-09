420d22097b528bef8aabfecc253edae7e8e0a8f4
Agent Hacker <43415449+AgentHackerYT@users.noreply.github.com>
2022-01-09 09:07:55 +0300

    Update Xiao.js

--- Xiao.js
+++ Xiao.js
@@ -64,45 +64,18 @@ client.on('ready', async () => {
 		{ text: () => `with ${formatNumber(client.registry.commands.size)} commands`, type: 'PLAYING' },
 		{ text: () => `${formatNumber(client.channels.cache.size)} channels`, type: 'WATCHING' }
 	);
-
-	// Interval to change activity every minute
 	client.setInterval(() => {
-		const activity = client.activities[Math.floor(Math.random() * client.activities.length)];
-		const text = typeof activity.text === 'function' ? activity.text() : activity.text;
-		client.user.setActivity(text, { type: activity.type });
+		client.user.setActivity("--help", { type: "PLAYING" });
 	}, 60000);
 
-	// Set up meme poster interval
-	if (client.memePoster) {
-		client.setInterval(async () => {
-			try {
-				const post = await client.memePoster.fetchRandomPost(false);
-				await client.memePoster.post(post);
-			} catch (err) {
-				client.logger.error(err);
-			}
-		}, client.memePoster.postInterval);
-	}
-
-	// Import command-leaderboard.json
-	try {
-		const results = client.importCommandLeaderboard();
-		if (!results) client.logger.error('[LEADERBOARD] command-leaderboard.json is not formatted correctly.');
-	} catch (err) {
-		client.logger.error(`[LEADERBOARD] Could not parse command-leaderboard.json:\n${err.stack}`);
-	}
-
-	// Export command-leaderboard.json every 30 minutes
-	client.setInterval(() => {
-		try {
-			client.exportCommandLeaderboard();
-		} catch (err) {
-			client.logger.error(`[LEADERBOARD] Failed to export command-leaderboard.json:\n${err.stack}`);
-		}
-	}, 1.8e+6);
 });
 
 client.on('message', async msg => {
+	if (msg.content.includes("@here") || msg.content.includes("@everyone")) return false;
+
+    if (msg.mentions.has(client.user.id)) {
+        msg.channel.send("Hi My Prefix is `--`\nGet Started by doing `--help`");
+    };
 	const hasText = Boolean(msg.content);
 	const hasImage = msg.attachments.size !== 0;
 	const hasEmbed = msg.embeds.length !== 0;
@@ -122,58 +95,6 @@ client.on('message', async msg => {
 	}
 });
 
-client.on('guildCreate', async guild => {
-	if (guild.systemChannel && guild.systemChannel.permissionsFor(client.user).has('SEND_MESSAGES')) {
-		try {
-			const usage = client.registry.commands.get('help').usage();
-			await guild.systemChannel.send(`Hi! I'm Xiao, use ${usage} to see my commands, yes?`);
-		} catch {
-			// Nothing!
-		}
-	}
-	const joinLeaveChannel = await client.fetchJoinLeaveChannel();
-	if (joinLeaveChannel) {
-		if (!guild.members.cache.has(guild.ownerID)) await guild.members.fetch(guild.ownerID);
-		const embed = new MessageEmbed()
-			.setColor(0x7CFC00)
-			.setThumbnail(guild.iconURL({ format: 'png' }))
-			.setTitle(`Joined ${guild.name}!`)
-			.setFooter(`ID: ${guild.id}`)
-			.setTimestamp()
-			.addField('❯ Members', formatNumber(guild.memberCount))
-			.addField('❯ Owner', guild.owner.user.tag);
-		await joinLeaveChannel.send({ embed });
-	}
-});
-
-client.on('guildDelete', async guild => {
-	const joinLeaveChannel = await client.fetchJoinLeaveChannel();
-	if (joinLeaveChannel) {
-		const embed = new MessageEmbed()
-			.setColor(0xFF0000)
-			.setThumbnail(guild.iconURL({ format: 'png' }))
-			.setTitle(`Left ${guild.name}...`)
-			.setFooter(`ID: ${guild.id}`)
-			.setTimestamp()
-			.addField('❯ Members', formatNumber(guild.memberCount));
-		await joinLeaveChannel.send({ embed });
-	}
-});
-
-client.on('guildMemberRemove', async member => {
-	if (member.id === client.user.id) return null;
-	if (member.partial) await member.fetch();
-	const channel = member.guild.systemChannel;
-	if (!channel || !channel.permissionsFor(client.user).has('SEND_MESSAGES')) return null;
-	if (channel.topic && channel.topic.includes('<xiao:disable-leave>')) return null;
-	try {
-		const leaveMessage = client.leaveMessages[Math.floor(Math.random() * client.leaveMessages.length)];
-		await channel.send(leaveMessage.replaceAll('{{user}}', `**${member.user.tag}**`));
-		return null;
-	} catch {
-		return null;
-	}
-});
 
 client.on('disconnect', event => {
 	client.logger.error(`[DISCONNECT] Disconnected with code ${event.code}.`);
